7ad43c23b85f12515594324c72349a850f8824f0
hanafindra <hanafindra@gmail.com>
2021-06-11 19:34:01 +0700

    renew app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./graphql/Settings";
